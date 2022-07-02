3c62367cbc81d0f975df9baa4be54ff1e6044ebc
Justus B <justus.c.brown@gmail.com>
2022-07-01 21:55:27 -0700

    local storage saving/rendering

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,4 +1,19 @@
 
+//get all save buttons
+var svBtn9 =  $("#save-button-9am")
+var svBtn10 =  $("#save-button-10am")
+var svBtn11=  $("#save-button-11am")
+var svBtn12 =  $("#save-button-12pm")
+var svBtn1 =  $("#save-button-1pm")
+var svBtn2 =  $("#save-button-2pm")
+var svBtn3 =  $("#save-button-3pm")
+var svBtn4 =  $("#save-button-4pm")
+var svBtn5 =  $("#save-button-5pm")
+
+
+
+
+
 function updateClock() { //function to update timer element
     var timerEl = $("#timer-slot")
     timerEl.text(moment().format("MMM Do, YYYY hh:mm:ss"))
@@ -34,4 +49,38 @@ function colorInputs(){
 
 colorInputs() //set colors on page load
 
-setInterval(colorInputs, 60000) //refresh colors on the minute
+setInterval(colorInputs, 60000); //refresh colors on the minute
+
+//save input values
+    //create an object to hold values
+    //listen for clicks on our save buttons
+        //save the data in the input to its place in the object
+        //save object in local storage
+
+var hourlyEvents = {};
+
+svBtn9.on("click", function(event){ // on click, get the value and save it 
+    event.preventDefault()
+    var inputJQ = $(hourInputArray[0]); //make it a JQuery object
+    var inputVal = inputJQ.val(); //save the value 
+    console.log(inputVal);
+    hourlyEvents.nineam = inputVal
+    localStorage.setItem("hourlyEventsStored", JSON.stringify(hourlyEvents)) //save object in local storage
+    console.log(hourlyEvents)
+})
+
+
+function renderSavedEvents(){
+    var hourlyEventsMidMan = JSON.parse(localStorage.getItem("hourlyEventsStored"))
+    console.log(hourlyEvents)
+    console.log(hourInputArray[0])
+    // if (hourlyEventsMidMan == ""){
+    //     return
+    // }
+    hourlyEvents = hourlyEventsMidMan
+    hourInputArray[0].textContent = hourlyEvents.nineam
+}
+renderSavedEvents()
+//render saved inputs on page load
+    // get from local storage
+    // set inputs to new value 
